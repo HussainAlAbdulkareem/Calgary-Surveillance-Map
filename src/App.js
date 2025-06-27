053a9b1cbab9e7101bec4f46a6922af3bbeaffc1
HussainAlAbdulkareem <hussain.alabdulkaree@ucalgary.ca>
2025-06-27 11:06:06 -0600

    Render Map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { MapContainer, TileLayer } from 'react-leaflet';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer
+  center={[51.0447, -114.0719]} // Calgary coordinates
+  zoom={13}
+  style={{ height: "100vh", width: "100%" }}
+>
+  <TileLayer
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+</MapContainer>
   );
 }
 
