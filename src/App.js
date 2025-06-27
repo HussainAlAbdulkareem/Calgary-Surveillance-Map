40b4e6a8695b3cfe584e659f60f6d480046f0a29
HussainAlAbdulkareem <hussain.alabdulkaree@ucalgary.ca>
2025-06-27 11:35:32 -0600

    Add Camera Pins on Map

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import { MapContainer, TileLayer } from 'react-leaflet';
+import { useEffect, useState } from "react";
+import { Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+
 
 
 function App() {
+  const [cameraData, setCameraData] = useState([]);
+
+
+  useEffect(() => {
+  fetch("https://data.calgary.ca/resource/k7p9-kppz.geojson")
+    .then((res) => res.json())
+    .then((data) => {
+      setCameraData(data.features);
+    })
+    .catch((err) => console.error("Error fetching camera data:", err));
+}, []);
+
   return (
+    
+    
     <MapContainer
   center={[51.0447, -114.0719]} // Calgary coordinates
   zoom={13}
@@ -13,6 +31,17 @@ function App() {
   <TileLayer
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
+
+  {cameraData.map((feature, idx) => {
+  const [lng, lat] = feature.geometry.coordinates;
+  const label = feature.properties.location;
+
+  return (
+    <Marker key={idx} position={[lat, lng]}>
+      <Popup>{label}</Popup>
+    </Marker>
+  );
+})}
 </MapContainer>
   );
 }
